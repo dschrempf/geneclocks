76b6bd6ed8f08e78db03e77de05aa3bb0ce1d2f3
Dominik Schrempf <dominik.schrempf@gmail.com>
2018-05-16 18:57:39 +0200

    Comments.

--- src/Geneclocks/Coalescent.hs
+++ src/Geneclocks/Coalescent.hs
@@ -23,6 +23,8 @@ import Geneclocks.Distribution.CoalescentContinuous
 import System.Random.MWC
 import Geneclocks.Tree.Phylo
 
+-- | Simulate a coalescent tree with 'n' leaves. The branch lengths are in units
+-- of effective population size.
 simulate :: (PrimMonad m)
          => Int -- ^ Number of leaves.
          -> Gen (PrimState m)
@@ -37,16 +39,21 @@ simulate' :: (PrimMonad m)
           -> [PhyloTree Int Double]
           -> Gen (PrimState m)
           -> m (PhyloTree Int Double)
-simulate' n a trs g | n <= 0                     = error "Cannot construct trees without leaves."
-                    | n == 1 && length trs /= 1  = error "Too many trees provided."
-                    | n == 1 && length trs == 1  = return $ head trs
-                    | otherwise                  =
-                      do
-                        i <- uniformR (1, n-1) g
-                        t <- genContVar (coalescentDistributionCont n) g
-                        let trs'  = map (lengthen t) trs
-                            tl    = trs' !! (i-1)
-                            tr    = trs' !! i
-                            tm    = glue (Info a 0.0 Internal) [tl, tr]
-                            trs'' = take (i-1) trs' ++ [tm] ++ drop (i+1) trs'
-                        simulate' (n-1) a trs'' g
+simulate' n a trs g
+  | n <= 0                     = error "Cannot construct trees without leaves."
+  | n == 1 && length trs /= 1  = error "Too many trees provided."
+  | n == 1 && length trs == 1  = return $ head trs
+  | otherwise                  =
+      do
+        -- Indices of the leaves to join will be i-1 and i.
+        i <- uniformR (1, n-1) g
+        -- The time of the coalescent event.
+        t <- genContVar (coalescentDistributionCont n) g
+        let trs'  = map (lengthen t) trs -- Move time 't' up on the tree.
+            tl    = trs' !! (i-1)
+            tr    = trs' !! i
+            -- Join the two chosen trees.
+            tm    = glue (Info a 0.0 Internal) [tl, tr]
+            -- Take the trees on the left, the merged tree, and the trees on the right.
+            trs'' = take (i-1) trs' ++ [tm] ++ drop (i+1) trs'
+        simulate' (n-1) a trs'' g
